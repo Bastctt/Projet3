ae9a2a9e0550256534a96c632ff9b772adc6b753
Bastien Cochet <bastien@MacBook-de-Bast.local>
2023-03-01 19:29:51 +0100

    ajout fonctionnalité mot de passe visible en js

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -3,6 +3,7 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   const email = document.querySelector('#email').value;
   const password = document.querySelector('#motDePasse').value;
+  const edit = document.querySelector('.edit');
   
   
   if (email === 'sophie.bluel@test.tld' && password === 'S0phie') {
@@ -22,6 +23,7 @@ form.addEventListener('submit', (event) => {
       console.log(data);
       localStorage.setItem('userToken', data.token)
       window.location.href = 'index.html';
+      edit.style.display = 'block';
     })
     .catch(error => {
       console.error(error);
@@ -31,5 +33,20 @@ form.addEventListener('submit', (event) => {
     alert('Email ou mot de passe incorrects');
   }
 });
+  
+const eye = document.querySelector(".feather-eye");
+const eyeoff = document.querySelector(".feather-eye-off");
+const passwordField = document.querySelector("input[type=password]");
 
+    eye.addEventListener("click", () => {
+      eye.style.display = "none";
+      eyeoff.style.display = "block";
+      passwordField.type = "text";
+    });
+
+    eyeoff.addEventListener("click", () => {
+      eyeoff.style.display = "none";
+      eye.style.display = "block";
+      passwordField.type = "password";
+    });
 
