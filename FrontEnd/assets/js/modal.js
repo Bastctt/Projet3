ff0e9d91cc636f18ec7bb14764b2509800d064c5
Bastien Cochet <bastien@MacBook-de-Bast.local>
2023-03-20 17:14:17 +0100

    avancement modal et ajouts d'effets

--- FrontEnd/assets/js/modal.js
+++ FrontEnd/assets/js/modal.js
@@ -1,16 +1,19 @@
-const editBtn = document.querySelector('.editBtn');
+const modalBody = document.querySelector('.modal-body');
+const edit3Btn = document.querySelector('.edit3Btn');
 const overlay = document.querySelector('.overlay');
 const modal = document.querySelector('.myModal');
 const closeBtn = document.querySelector('.closeBtn');
 
-editBtn.addEventListener('click', function () {
+edit3Btn.addEventListener('click', function () {
   overlay.classList.add('visible');
   modal.classList.add('visible');
+});
 
 closeBtn.addEventListener('click', function () {
   modal.classList.remove('visible');
   overlay.classList.remove('visible');
 });
 
-});
+
+
 
